f129e391d85dc0724f7627acce14a2c4b9047538
MarioRM12 <xtyponx@gmail.com>
2024-01-22 14:36:47 +0100

    22 de enero

---
+++ src/main/java/org/iesvdm/dao/PedidoDAO.java
@@ -0,0 +1,25 @@
+package org.iesvdm.dao;
+
+import org.iesvdm.modelo.Cliente;
+import org.iesvdm.modelo.Comercial;
+import org.iesvdm.modelo.Pedido;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PedidoDAO {
+
+    public void create(Pedido pedido);
+
+    void create(Cliente cliente);
+
+    public List<Pedido> getAll();
+    public Optional<Pedido> find(int id);
+
+    public void update(Pedido pedido);
+
+    public void delete(long id);
+
+    public List<Pedido> listaPedidosPorComercial(Comercial comercial);
+
+}
